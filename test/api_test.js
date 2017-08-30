051c6e27121112b971bcbcb935e3ee154289e2c1
YT <tate_fan@163.com>
2017-08-30 13:03:37 +0800

    test pass

--- test/api_test.js
+++ test/api_test.js
@@ -1,4 +1,14 @@
 'use strict';
-const ava = require('ava');
-const agent = require('superagent');
+const agent = require('supertest');
+const assert = require('assert');
 const app = require('../app/main');
+
+
+describe('##/api/v1/ # home', function() {
+  it('#Should alive', (done) => {
+    agent(app)
+    .get('/api/v1')
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .expect(200, done);
+  });
+});
